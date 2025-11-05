f272bc0cbff5efba633d0c38dbbc13aff8be7761
uwu-gl <moegluwu0@gmail.com>
2025-11-05 22:53:30 +0800

    lineage: Guard some properties with WITH_GMS
    Change-Id: I5956632433cb60125ca07101626cb0a69004b663
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -225,9 +225,11 @@ endif
 $(call inherit-product, vendor/lineage/audio/audio.mk)
 
 # SetupWizard
+ifneq ($(WITH_GMS), true)
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v4 \
     setupwizard.feature.day_night_mode_enabled=true
+endif
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
