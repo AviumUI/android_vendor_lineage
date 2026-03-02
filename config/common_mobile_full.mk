0bf766fbed3355d9cc7d7f23cc31b1215d8933b2
Michael Bestas <mkbestas@gmail.com>
2026-03-02 15:36:14 +0200

    config: Remove Rubik/Lato fonts for now
    Fonts customization is not ported yet on 16 QPR2 and the new font style
    in M3E requires way more extensive changes in frameworks and apps.
    We will just set the default to google-sans-flex for now to match the
    new look and we can potentially revisit font customization in the
    future.
    
    Change-Id: I98a22c8dc6de0a299ffffa04deb3c80dc171d1d9

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,10 +3,8 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {GoogleSansFlex,Lato,Rubik} fonts
+# Include GoogleSansFlex font
 $(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 # Apps
 PRODUCT_PACKAGES += \
