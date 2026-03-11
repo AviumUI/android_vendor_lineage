998ddfffd634848c1d73b04a67122c3e1a3c669c
uwu-gl <moegluwu0@gmail.com>
2026-03-12 01:21:00 +0800

    lineage: Only build fonts_customization.xml
    Change-Id: Ibdcd9a955d5c9569ac019ba3c82c1bd8e2ffe36c
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -29,10 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Fonts
 PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontGoogleSansFlexOverlay \
-    FontLatoOverlay \
-    FontRubikOverlay
+    fonts_customization.xml
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
